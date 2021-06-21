8ca7cb75162842c918b1ccf3b2e5e35b61bc05a8
AGs <agnesarong@gmail.com>
2021-06-21 21:11:01 +0100

    created a simple interest calculator

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+var p,r,t,si;
+function calc()
+{
+p = document.getElementById("p").value;
+r = document.getElementById("r").value;
+t = document.getElementById("t").value;
+si = document.getElementById("si");
+Simple Interest.innerHTML= "Simple Interest" + (p*r*t/100);
+}
+    
